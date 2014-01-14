741162f0aaf19c56185edd4c2aecc096f967c522
Fernando Rodriguez Sela <frsela@tid.es>
2014-01-14 07:20:45 +0100

    A ReadOnly jsDAVlib version working

---
+++ src/jsDAVConnection.js
@@ -0,0 +1,96 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+'use strict';
+
+// webdav params: url, user, password
+function jsDAVConnection(params) {
+  this.params = {
+    url: 'http://test.webdav.org/dav/',
+    user: '',
+    password: ''
+  };
+
+  this.server = {
+    DAV: undefined,
+    Allow: undefined
+  };
+
+  if (params.url) {
+    this.params.url = params.url;
+  }
+  if (params.user && params.password) {
+    this.params.user = params.user;
+    this.params.password = params.password;
+  }
+
+  // Recover ROOT folder resource
+  var self = this;
+  jsDAVlib.checkRepository(this, function(props, error) {
+    if (error) {
+      jsDAVlib_debug('ERROR: ' + error);
+      return self.onerror(error);
+    }
+
+    self.server = props;
+    jsDAVlib_debug('Server ' + self.params.url + ' capacities', self.server);
+    jsDAVlib.getResource(self, '', function(DAVResource, error) {
+      self.rootResource = DAVResource;
+
+      // FIX Base URL
+      // All resourceURL will be relative to the base path. We can guess this
+      // base path based on the href returned by the first element of the root
+      // resource
+      var absoluteURL = self.params.url;
+      var relativeURL = DAVResource.data.items[0].href;
+      if (absoluteURL.substr(-1) != relativeURL.substr(-1)) {
+        if (relativeURL.substr(-1) === '/')
+          absoluteURL += relativeURL.substr(-1);
+      }
+      self.params.url = absoluteURL.substr(0, absoluteURL.indexOf(relativeURL));
+
+      self.onready();
+    });
+  });
+}
+
+jsDAVConnection.prototype = {
+  onready: function __override_me_onready__() {},
+  onerror: function __override_me_onerror__() {},
+
+  isAddressBook: function isAddressBook() {
+    return this.rootResource.isAddressBook();
+  },
+  isCalendar: function isCalendar() {
+    return this.rootResource.isCalendar();
+  },
+
+  getInfo: function getInfo() {
+    if (this.rootResource.isException()) return {
+      url: this.params.url,
+      error: 'Invalid DAV resource'
+    };
+    return {
+      url: this.params.url,
+      lastModified: this.rootResource.data.items[0],
+      isDAVResource: !this.rootResource.isException(),
+      isAddressBook: this.isAddressBook(),
+      isCalendar: this.isCalendar()
+    }
+  },
+
+  // If resourceURL is null, then root Resource is returned
+  getResource: function getContentsList(resourceURL, callback) {
+    if (typeof(callback) !== 'function')
+      callback = function __dummy_callback__() {};
+
+    if (resourceURL === null) {
+      callback(this.rootResource);
+    } else {
+      jsDAVlib.getResource(this, resourceURL, function(DAVResource, error) {
+        callback(DAVResource);
+      });
+    }
+  }
+};
---
+++ src/jsDAVResource.js
@@ -0,0 +1,91 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+'use strict';
+
+function jsDAVResource(XMLDocument) {
+  this.xml = XMLDocument;
+  this.data = jsDAVXMLParser.parse(XMLDocument);
+}
+
+jsDAVResource.prototype = {
+  addFileContents: function addFileContents(data) {
+    this.contents = data;
+  },
+  isFile: function isFile() {
+    if (!this.data.valid)
+      return false;
+    return this.data.items[0].resourceType.type === 'file';
+  },
+  isCollection: function isCollection() {
+    if (!this.data.valid)
+      return false;
+    return this.data.items[0].resourceType.type === 'dir';
+  },
+  isAddressBook: function isAddressBook() {
+    if (!this.data.valid)
+      return false;
+    return this.data.items[0].resourceType.addressbook === true;
+  },
+  isCalendar: function isCalendar() {
+    if (!this.data.valid)
+      return false;
+    return this.data.items[0].resourceType.calendar === true;
+  },
+  isException: function isException() {
+    if (!this.data.valid)
+      return 'Received data is not valid';
+    return false;
+  },
+  getExceptionInfo: function getExceptionInfo() {
+    // TO-DO
+  },
+
+  getMetadata: function getMetadata() {
+    return {
+      href: this.data.items[0].href,
+      mime: this.data.items[0].mime,
+      size: this.data.items[0].size,
+      type: this.data.items[0].resourceType
+    }
+  },
+  getFileContents: function getFileContents() {
+    if (this.isFile && this.contents)
+      return this.contents;
+    else
+      return null;
+  },
+  getCollectionContents: function getCollectionContents() {
+    if (this.isCollection) {
+      var list = [];
+      for (var i=1; i<this.data.items.length; i++) {
+        list.push({
+          href: this.data.items[i].href,
+          mime: this.data.items[i].mime,
+          size: this.data.items[i].size,
+          lastModified: this.data.items[i].lastModified,
+          type: this.data.items[i].resourceType.type
+        });
+      }
+      return list;
+    } else
+      return null;
+  },
+
+  // Collection resource responds with an array of all his elements
+  // File resource responds with the file contents
+  get: function get() {
+    if (this.isFile() || this.contents) {
+      return {
+        meta: this.getMetadata(),
+        data: this.getFileContents()
+      };
+    } else {
+      return {
+        meta: this.getMetadata(),
+        data: this.getCollectionContents()
+      };
+    }
+  }
+};
